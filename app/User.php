27454275f9bb8fcdd50bf6d317fe623a0ea429cb
Diego Felix <diegoflx.oliveira@gmail.com>
2019-06-04 21:11:25 -0300

    fix relationships

--- app/User.php
+++ app/User.php
@@ -5,7 +5,6 @@ namespace MercadoPago;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use MercadoPago\Address;
 
 class User extends Authenticatable
 {
